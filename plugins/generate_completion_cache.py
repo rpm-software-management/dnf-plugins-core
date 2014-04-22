9a6a7de93ac4484bd6c963a3dc4bf12d9704173d
Tim Lauridsen <timlau@fedoraproject.org>
2014-04-22 09:34:53 +0200

    generate-completion-cache: fix shared lib name

--- plugins/generate_completion_cache.py
+++ plugins/generate_completion_cache.py
@@ -13,7 +13,7 @@
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-from dnfplugins import logger
+from dnfpluginscore import logger
 
 import dnf
 import os.path
