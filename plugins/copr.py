56bf51484f756babe069f6f110df100ac40eda63
Michael Mraka <michael.mraka@redhat.com>
2015-01-29 13:32:05 +0100

    updated copyright

--- plugins/copr.py
+++ plugins/copr.py
@@ -1,6 +1,6 @@
 # supplies the 'copr' command.
 #
-# Copyright (C) 2014  Red Hat, Inc.
+# Copyright (C) 2014-2015  Red Hat, Inc.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
