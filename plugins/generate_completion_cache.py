rpm-software-management/dnf-plugins-core

commit e020c962928d9a9453674c82f95ea7eb36a44dc2

generate_completion_cache: store NEVRA insted of NA (RhBug:1226663)
Reported-by: Kostya Vasilyev <kmansoft@gmail.com>
Reference: https://bugzilla.redhat.com/show_bug.cgi?id=1226663
Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

plugins/generate_completion_cache.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # generate_completion_cache.py - generate cache for dnf bash completion
 # Copyright © 2013 Elad Alfassa <elad@fedoraproject.org>
-# Copyright (C) 2014 Igor Gnatenko <i.gnatenko.brain@gmail.com>
+# Copyright (C) 2014-2015 Igor Gnatenko <i.gnatenko.brain@gmail.com>
 # Copyright (C) 2015  Red Hat, Inc.
 
 # This copyrighted material is made available to anyone wishing to use,
@@ -61,8 +61,7 @@ class BashCompletionCache(dnf.Plugin):
                         "pkg_available ON available(pkg)")
                     cur.execute("delete from available")
                     avail_pkgs = self.base.sack.query().available()
-                    avail_pkgs_insert = [["{}.{}".format(x.name, x.arch)]
-                                         for x in avail_pkgs if x.arch != "src"]
+                    avail_pkgs_insert = [str(x) for x in avail_pkgs if x.arch != "src"]
                     cur.executemany("insert or ignore into available values (?)",
                                     avail_pkgs_insert)
                     conn.commit()
@@ -81,8 +80,7 @@ class BashCompletionCache(dnf.Plugin):
                     "pkg_installed ON installed(pkg)")
                 cur.execute("delete from installed")
                 inst_pkgs = self.base.sack.query().installed()
-                inst_pkgs_insert = [["{}.{}".format(x.name, x.arch)]
-                                    for x in inst_pkgs if x.arch != "src"]
+                inst_pkgs_insert = [str(x) for x in inst_pkgs if x.arch != "src"]
                 cur.executemany("insert or ignore into installed values (?)",
                                 inst_pkgs_insert)
                 conn.commit()

commit 8ee33171f7e6967bee6c71666beda088a11ab08c

generate_completion_cache: use list for each insert (fixes regression introduced in e020c96)
Traceback (most recent call last):
  File "/home/jsilhan/Documents/dnf/bin/dnf-2", line 36, in <module>
    main.user_main(sys.argv[1:], exit_code=True)
  File "/home/jsilhan/Documents/dnf/dnf/cli/main.py", line 193, in user_main
    errcode = main(args)
  File "/home/jsilhan/Documents/dnf/dnf/cli/main.py", line 84, in main
    return _main(base, args)
  File "/home/jsilhan/Documents/dnf/dnf/cli/main.py", line 146, in _main
    ret = resolving(cli, base)
  File "/home/jsilhan/Documents/dnf/dnf/cli/main.py", line 178, in resolving
    base.plugins.run_transaction()
  File "/home/jsilhan/Documents/dnf/dnf/plugin.py", line 82, in fn
    dnf.util.mapall(operator.methodcaller(method), self.plugins)
  File "/home/jsilhan/Documents/dnf/dnf/util.py", line 157, in mapall
    return list(map(fn, *seq))
  File "/home/jsilhan/Documents/dnf-plugins-core/plugins/generate_completion_cache.py", line 85, in transaction
    inst_pkgs_insert)
sqlite3.ProgrammingError: Incorrect number of bindings supplied. The current statement uses 1, and there are 29 supplied.

Reported-by: Jan Silhan <jsilhan@redhat.com>
Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

plugins/generate_completion_cache.py
@@ -61,7 +61,7 @@ class BashCompletionCache(dnf.Plugin):
                         "pkg_available ON available(pkg)")
                     cur.execute("delete from available")
                     avail_pkgs = self.base.sack.query().available()
-                    avail_pkgs_insert = [str(x) for x in avail_pkgs if x.arch != "src"]
+                    avail_pkgs_insert = [[str(x)] for x in avail_pkgs if x.arch != "src"]
                     cur.executemany("insert or ignore into available values (?)",
                                     avail_pkgs_insert)
                     conn.commit()
@@ -80,7 +80,7 @@ class BashCompletionCache(dnf.Plugin):
                     "pkg_installed ON installed(pkg)")
                 cur.execute("delete from installed")
                 inst_pkgs = self.base.sack.query().installed()
-                inst_pkgs_insert = [str(x) for x in inst_pkgs if x.arch != "src"]
+                inst_pkgs_insert = [[str(x)] for x in inst_pkgs if x.arch != "src"]
                 cur.executemany("insert or ignore into installed values (?)",
                                 inst_pkgs_insert)
                 conn.commit()