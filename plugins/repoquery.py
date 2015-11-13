28a4ebc95ea90370436a7a5a217c919cbda15ce7
Michal Luscon <mluscon@redhat.com>
2015-11-13 14:14:25 +0100

    repoquery: fix unicode tracebacks

--- plugins/repoquery.py
+++ plugins/repoquery.py
@@ -444,8 +444,8 @@ class PackageWrapper(object):
     def __getattr__(self, attr):
         atr = getattr(self._pkg, attr)
         if isinstance(atr, list):
-            return '\n'.join(sorted([str(reldep) for reldep in atr]))
-        return str(atr)
+            return '\n'.join(sorted([dnf.i18n.ucd(reldep) for reldep in atr]))
+        return atr
 
     @staticmethod
     def _get_timestamp(timestamp):
