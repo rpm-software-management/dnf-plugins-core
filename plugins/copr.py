4812d83380bf41361eb5fac4d7aacac523870561
Michael Mraka <michael.mraka@redhat.com>
2016-05-05 11:02:03 +0200

    removed unused args parameter from configure() and run()

--- plugins/copr.py
+++ plugins/copr.py
@@ -91,7 +91,7 @@ class CoprCommand(dnf.cli.Command):
                                      'remove', 'list', 'search'])
         parser.add_argument('arg', nargs='*')
 
-    def configure(self, args):
+    def configure(self):
         raw_config = ConfigParser()
         filepath = os.path.join(os.path.expanduser("~"), ".config", "copr")
         if raw_config.read(filepath):
@@ -120,7 +120,7 @@ class CoprCommand(dnf.cli.Command):
             else:
                 self.chroot_config = [False, False]
 
-    def run(self, extcmds):
+    def run(self):
         subcommand = self.opts.subcommand[0]
         if subcommand == "help":
             self.cli.optparser.print_help(self)
@@ -430,7 +430,7 @@ Do you want to continue? [y/N]: """)
         parser.add_argument('subcommand', nargs=1,
                             choices=['enable', 'disable', 'upgrade'])
 
-    def run(self, extcmds):
+    def run(self):
         subcommand = self.opts.subcommand[0]
         chroot = self._guess_chroot()
         if subcommand == "enable":
