20fd271eff6ab32fc0516a5bed0c421cedd77bca
Jaroslav Mracek <jmracek@redhat.com>
2017-03-14 13:24:46 +0100

    Create dir for local plugin if path not exist
    It creates directory for local repository if path doesn't exist.
    
    This is port of commit c20ca34df9ebca8714c7d347db98db392ffe9dc0 from
    dnf-plugins-extras.

--- plugins/local.py
+++ plugins/local.py
@@ -21,6 +21,7 @@
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
+from dnf.i18n import ucd
 from dnfpluginscore import _, logger
 
 import dnf
@@ -108,7 +109,14 @@ class Local(dnf.Plugin):
             return
 
         repodir = main["repodir"]
-        if not os.path.isdir(repodir):
+        if not os.path.exists(repodir):
+            try:
+                os.makedirs(repodir, mode=0o755)
+            except OSError as e:
+                self.logger.error("local: " + _(
+                    "Unable to create a directory '{}' due to '{}'").format(repodir, ucd(e)))
+                return
+        elif not os.path.isdir(repodir):
             self.logger.error(
                 "local: " + _("'{}' is not a directory").format(repodir))
             return
