26e3307b559b898aeb34bcf33cf644b8c852e14e
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2019-05-15 10:39:03 +0430

    Set the _dnf_local repository cost to 500
    * Make _dnf_local repository the preferred repository compared to normal
      repositories

--- plugins/local.py
+++ plugins/local.py
@@ -103,6 +103,7 @@ class Local(dnf.Plugin):
 
         local_repo = dnf.repo.Repo("_dnf_local", self.base.conf)
         local_repo.baseurl = "file://{}".format(self.main["repodir"])
+        local_repo.cost = 500
         self.base.repos.add(local_repo)
 
     def transaction(self):
